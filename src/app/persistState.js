6da54fefb6a8e7339f2bda9bae07c25d0aa99676
scottzyang <scottzyang@gmail.com>
2024-02-14 22:08:36 -0600

    Add store

---
+++ src/app/persistState.js
@@ -0,0 +1,22 @@
+const PSSWRDZ_STATE = "PSSWRDZ_STATE"
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem(PSSWRDZ_STATE)
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch(err) {
+    return undefined
+  }
+}
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem(PSSWRDZ_STATE, serializedState)
+  } catch(err) {
+    console.log("Error saving data:"+err)
+  }
+}
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import passwordsReducer from '../features/passwords/passwordSlice'
+import { saveState, loadState } from './persistState';
 
 export const store = configureStore({
   reducer: {
     passwords: passwordsReducer
   },
+  preloadedState: loadState()
+})
+
+
+store.subscribe(() => {
+  saveState(store.getState());
 })
